e85c56eb09b6744bf8ff2bac5ff88f91910db891
Jim Razmus II <jim.razmus@code42.com>
2019-02-10 12:53:00 -0600

    Add helper script for working on pipeline.

---
+++ .circleci/run-build-locally.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+curl --user $CIRCLECI_USER \
+    --request POST \
+    --form revision=$CIRCLECI_COMMIT \
+    --form config=@config.yml \
+    --form notify=false \
+        https://circleci.com/api/v1.1/project/github/jimrazmus/ami-ledger/tree/master
