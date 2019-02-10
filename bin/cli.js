6c025080df3540fbe82edf71b32ac9da832903de
Jim Razmus II <jim.razmus@code42.com>
2019-02-10 12:15:05 -0600

    Add the cli command.

--- bin/cli.js
+++ bin/cli.js
@@ -8,10 +8,19 @@ const cmd = require("commander");
 const log = require("loglevel");
 
 const amis = require("../src/amis.js");
+const cva = require("../src/cmd-validate-auth.js");
 
 cmd
   .description('it does stuff')
   .option("-l, --loglevel <level>", "Set logging level", /^(trace|debug|info|warn|error)$/i, "error")
   .version("1.0.0");
+  
+cmd
+  .command('auth')
+  .description('Test authentication via STS getCallerIdentity')
+  .action(function(){
+    cva.confirmCreds(cmd.loglevel);
+  });
+
 
 cmd.parse(process.argv);
