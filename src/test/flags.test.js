1a772d74af615cf48d798fe9d0f9cab68a29de88
Jim Razmus II <jim.razmus@code42.com>
2019-02-11 09:11:28 -0600

    Add a flags module.

---
+++ src/test/flags.test.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const flags = require("../flags.js");
+
+const ALL = flags.ADD | flags.REMOVE;
+
+test("Add flag is on", () => {
+    var flag = 0 | flags.ADD;
+    expect(flags.isAddSet(flag)).toBe(true);
+  });
+
+test("Add flag is off", () => {
+  var flag = 0;
+  expect(flags.isAddSet(flag)).toBe(false);
+});
+
+test("Remove flag is on", () => {
+  var flag = 0 | flags.REMOVE;
+  expect(flags.isRemoveSet(flag)).toBe(true);
+});
+
+test("Remove flag is off", () => {
+  var flag = 0;
+  expect(flags.isRemoveSet(flag)).toBe(false);
+});
+
+test("All flags are on", () => {
+  var flag = 0 | flags.ADD | flags.REMOVE;
+  expect(flags.isAllSet(flag)).toBe(true);
+});
+
+test("All flags are not on", () => {
+  var flag = 0;
+  expect(flags.isAllSet(flag)).toBe(false);
+});
