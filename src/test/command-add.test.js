1ff26aef6942a78de9057d1703acf8d5d59d12eb
Jim Razmus II <jim.razmus@code42.com>
2019-02-12 15:55:36 -0600

    Call the main routine from cli and remove redundant/obsolete files.

--- src/test/command-add.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const commandAdd = require("../command-add.js");
-
-test("expect command add to throw", () => {
-  expect(commandAdd.add).toThrow();
-});
--- src/test/command-full.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-const commandFull = require("../command-full.js");
-
-test("expect command full to throw", () => {
-  expect(commandFull.full).toThrow();
-});
